0ca16703137332b07aee80ca5a57eb24b29bc3bb
lolxnn <inkyincolto@gmail.com>
2019-11-29 08:51:59 +0100

    Add files via upload

---
+++ Bastone.ino
@@ -0,0 +1,64 @@
+#include <RH_ASK.h>
+#include <SPI.h>
+#define TRIG 6
+#define ECHO 5
+#define BUZZER 13
+#define MOTOR 10
+#define BUTHOME 9
+#define BUTGROUND 8
+long durata;
+long dist;
+long distanza;
+long newdistanza;
+int i;
+RH_ASK driver;
+
+void setup() {
+  pinMode(ECHO, INPUT);
+  pinMode(TRIG, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(MOTOR, OUTPUT);
+  Serial.begin(9600);
+  if (!driver.init()) Serial.println("init failed");
+}
+
+long checkDistanza(){
+  digitalWrite(TRIG, LOW);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  durata = pulseIn(ECHO, HIGH);
+  dist = 0.034 * durata / 2;
+  return dist;
+}
+
+void sendHome(){
+    const char *msgH = "GOHOME";
+    driver.send((uint8_t *)msgH, strlen(msgH));
+    driver.waitPacketSent();
+    
+}
+
+void sendGround(){
+    const char *msgG = "GROUND";
+    driver.send((uint8_t *)msgG, strlen(msgG));
+    driver.waitPacketSent();
+    
+}
+
+void loop() {
+  distanza = checkDistanza();
+  Serial.println(distanza);
+  for(i = 0; i < distanza; i++){
+    newdistanza = checkDistanza();
+    if((newdistanza - 3) > distanza || (newdistanza + 3) < distanza) break;
+    if(distanza < 40 ) digitalWrite(MOTOR, HIGH);
+    else digitalWrite(MOTOR, LOW);
+    if(digitalRead(BUTHOME)) sendHome();
+    if(digitalRead(BUTGROUND)) sendGround();
+    delay(1);
+  }
+  tone(BUZZER, 500, 70);
+  delay(70);
+  noTone(BUZZER);
+}
---
+++ Ricevitore.ino
@@ -0,0 +1,33 @@
+#include <RH_ASK.h>
+#include <SPI.h>
+#define HOME 12
+#define GROUND 11
+RH_ASK driver;
+
+void setup()
+{
+    pinMode(HOME, OUTPUT);
+    pinMode(GROUND, OUTPUT);
+    Serial.begin(9600);  
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop(){
+    uint8_t buf[6];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)) 
+    {
+      int i;  
+      if((char*)buf == "HOME"){
+          digitalWrite(HOME, HIGH);
+          delay(500);
+          digitalWrite(HOME, LOW);
+        }
+      if((char*)buf == "GROUND"){
+          digitalWrite(GROUND, HIGH);
+          delay(500);
+          digitalWrite(GROUND, LOW);
+        }         
+    }
+}
